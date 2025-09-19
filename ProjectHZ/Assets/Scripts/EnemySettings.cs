4d847c47ba96331c5a47e8524286943bd0bab069
Fox <mike309010@gmail.com>
2025-09-19 08:00:56 +0300

    frat: global building update

--- ProjectHZ/Assets/Scripts/EnemySettings.cs
+++ ProjectHZ/Assets/Scripts/EnemySettings.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class EnemySettings : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
     private int _currentHealth;
+
+    [SerializeField] private GameObject _healthBar;
+    [SerializeField] private Image _healthBarImage;
+
 
     private WaveSpawner _waveSpawner;
     private LineEnemyDetector _lineEnemyDetector;
@@ -14,11 +19,16 @@ public class EnemySettings : MonoBehaviour
         _currentHealth = _maxHealth;
         _waveSpawner = WaveSpawner.Instance;
         _lineEnemyDetector = _waveSpawner.LineControllers[(int)(transform.position.z / 2)];
+        _healthBar.SetActive(false);
     }
 
     public void ReceiveDamage(int damage)
     {
+        if (_currentHealth == _maxHealth)
+            _healthBar.gameObject.SetActive(true);
+
         _currentHealth -= damage;
+        _healthBarImage.fillAmount = (float )_currentHealth / (float) _maxHealth;
         if (_currentHealth < 1)
             Destroy(this.gameObject);
     }
